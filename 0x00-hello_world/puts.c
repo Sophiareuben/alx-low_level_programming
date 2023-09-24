6e778e6e8ac9ba0a175101ab36a364ba5002de0f
sophuareuben <sophiareuben21@gmail.com>
2023-09-24 13:10:07 -0700

    Project Hello World

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - This is the  Entry point
+ *
+ * Return: Always Return 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+

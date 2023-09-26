28149c083ecaddc3001c449e452d229346f3ed16
sophiareuben <sophiareuben21@gmail.com>
2023-09-26 04:12:01 -0700

    debugging done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+/**
+* main - To Test function for positive or negative
+* Return: 0
+* Written by Sophiareuben
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
